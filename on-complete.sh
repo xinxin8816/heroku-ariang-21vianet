553104dc18d687afef0c7af89cd7cb09a31ad121
Zealer Chen <xinxin8816@gmail.com>
2020-04-19 10:34:43 +0800

    Optimize Script
    Delete useless configuration and description, use heroku-friendly commands and add English descriptions.

--- on-complete.sh
+++ on-complete.sh
@@ -1,60 +1,49 @@
-#!/usr/bin/env bash
-#============================================================
-# https://github.com/P3TERX/aria2.conf
-# File name：autoupload.sh
-# Description: Aria2 download completes calling Rclone upload
-# Lisence: MIT
-# Version: 2.0
-# Author: P3TERX
-# Blog: https://p3terx.com
-#============================================================
+#!/bin/bash
+# Aria2 Autoupload.sh by P3TERX, adapt Heroku-AriaNG by xinxin8816 and Kwok1am.
 
-## 基础设置 ##
-
-# Aria2下载目录
-# Aria2 一键安装管理脚本使用选项统一进行修改。
-# Aria2 Pro Docker 镜像无需修改，通过目录映射进行设置。
-DOWNLOAD_PATH='downloads'
-
-## 文件过滤 ##
+## 文件过滤 File Filter ##
 
 # 限制最低上传大小，仅 BT 多文件下载时有效，用于过滤无用文件。低于此大小的文件将被删除，不会上传。
+# Limit the minimum upload size, which is only valid when downloading multiple BT files, and is used to filter useless files. Files below this size will be deleted and will not be uploaded.
 #MIN_SIZE=10m
 
 # 保留文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。其它文件将被删除，不会上传。
+# Keep the file type, only effective when downloading multiple BT files, used to filter useless files. Other files will be deleted and will not be uploaded.
 #INCLUDE_FILE='mp4,mkv,rmvb,mov'
 
 # 排除文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。排除的文件将被删除，不会上传。
+# Exclude file types, valid only when downloading multiple BT files, used to filter useless files. Excluded files will be deleted and will not be uploaded.
 #EXCLUDE_FILE='html,url,lnk,txt,jpg,png'
 
-## 高级设置 ##
-
-# RCLONE 配置文件路径
-#export RCLONE_CONFIG=$HOME/.config/rclone/rclone.conf
-
-# RCLONE 配置文件密码
-#export RCLONE_CONFIG_PASS=password
+## 高级设置 advanced settings ##
 
 # RCLONE 并行上传文件数，仅对单个任务有效。
+# RCLONE The number of files uploaded in parallel is only valid for a single task.
 #export RCLONE_TRANSFERS=4
 
 # RCLONE 块的大小，默认5M，理论上是越大上传速度越快，同时占用内存也越多。如果设置得太大，可能会导致进程中断。
+# RCLONE The size of the block, the default is 5M. Theoretically, the larger the upload speed, the faster it will occupy more memory. If the setting is too large, the process may be interrupted.
 #export RCLONE_CACHE_CHUNK_SIZE=5M
 
 # RCLONE 块可以在本地磁盘上占用的总大小，默认10G。
+# RCLONE The total size that the block can occupy on the local disk, the default is 10G.
 #export RCLONE_CACHE_CHUNK_TOTAL_SIZE=10G
 
 # RCLONE 上传失败重试次数，默认 3
+# RCLONE Upload failed retry count, default 3
 #export RCLONE_RETRIES=3
 
 # RCLONE 上传失败重试等待时间，默认禁用，单位 s, m, h
+# RCLONE Upload failure retry wait time, disabled by default, unit s, m, h
 export RCLONE_RETRIES_SLEEP=30s
 
 # RCLONE 异常退出重试次数
+# RCLONE Abnormal exit retry count
 RETRY_NUM=3
 
 #============================================================
 
+DOWNLOAD_PATH='downloads'
 FILE_PATH=$3                                          # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
 REMOVE_DOWNLOAD_PATH=${FILE_PATH#${DOWNLOAD_PATH}/}   # 路径转换，去掉开头的下载路径。
 TOP_PATH=${DOWNLOAD_PATH}/${REMOVE_DOWNLOAD_PATH%%/*} # 路径转换，BT下载文件夹时为顶层文件夹路径，普通单文件下载时与文件路径相同。
@@ -73,7 +62,8 @@ TASK_INFO() {
 ${LIGHT_PURPLE_FONT_PREFIX}Download path:${FONT_COLOR_SUFFIX} ${DOWNLOAD_PATH}
 ${LIGHT_PURPLE_FONT_PREFIX}File path:${FONT_COLOR_SUFFIX} ${FILE_PATH}
 ${LIGHT_PURPLE_FONT_PREFIX}Upload path:${FONT_COLOR_SUFFIX} ${UPLOAD_PATH}
-${LIGHT_PURPLE_FONT_PREFIX}Remote path:${FONT_COLOR_SUFFIX} ${REMOTE_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}Remote path A:${FONT_COLOR_SUFFIX} ${REMOTE_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}Remote path B:${FONT_COLOR_SUFFIX} ${REMOTE_PATH_2}
 -------------------------- [${YELLOW_FONT_PREFIX}TASK INFO${FONT_COLOR_SUFFIX}] --------------------------
 "
 }
@@ -93,7 +83,8 @@ UPLOAD_FILE() {
             echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
             echo
         )
-        rclone move -v "${UPLOAD_PATH}" "${REMOTE_PATH}" --config="rclone.conf"
+        rclone copy -v "${UPLOAD_PATH}" "${REMOTE_PATH}" --config="rclone.conf"
+        rclone copy -v "${UPLOAD_PATH}" "${REMOTE_PATH_2}" --config="rclone.conf"
         RCLONE_EXIT_CODE=$?
         if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
             [ -e "${DOT_ARIA2_FILE}" ] && rm -vf "${DOT_ARIA2_FILE}"
@@ -109,6 +100,7 @@ UPLOAD_FILE() {
             )
             sleep 3
         fi
+		rclone delete -v "${UPLOAD_PATH}"
     done
 }
 
@@ -126,26 +118,6 @@ elif [ $2 -eq 0 ]; then
     exit 0
 fi
 
-echo
-echo -e "
------------------------------------------------------------
-    _         _          _   _       _                 _ 
-   / \  _   _| |_ ___   | | | |_ __ | | ___   __ _  __| |
-  / _ \| | | | __/ _ \  | | | | '_ \| |/ _ \ / _\` |/ _\` |
- / ___ \ |_| | || (_) | | |_| | |_) | | (_) | (_| | (_| |
-/_/   \_\__,_|\__\___/   \___/| .__/|_|\___/ \__,_|\__,_|
-                              |_|
-https://github.com/P3TERX/aria2.conf
-File name：autoupload.sh
-Description: Aria2 download completes calling Rclone upload
-Lisence: MIT
-Version: 2.0
-Author: P3TERX
-Blog: https://p3terx.com
------------------------------------------------------------
-"
-echo
-
 if [ -e "${FILE_PATH}.aria2" ]; then
     DOT_ARIA2_FILE="${FILE_PATH}.aria2"
 elif [ -e "${TOP_PATH}.aria2" ]; then
@@ -155,17 +127,20 @@ fi
 if [ "${TOP_PATH}" = "${FILE_PATH}" ] && [ $2 -eq 1 ]; then # 普通单文件下载，移动文件到设定的网盘文件夹。
     UPLOAD_PATH="${FILE_PATH}"
     REMOTE_PATH="${RCLONE_DESTINATION}"
+    REMOTE_PATH_2="${RCLONE_DESTINATION_2}"
     UPLOAD
     exit 0
 elif [ "${TOP_PATH}" != "${FILE_PATH}" ] && [ $2 -gt 1 ]; then # BT下载（文件夹内文件数大于1），移动整个文件夹到设定的网盘文件夹。
     UPLOAD_PATH="${TOP_PATH}"
     REMOTE_PATH="${RCLONE_DESTINATION}${REMOVE_DOWNLOAD_PATH%%/*}"
+	REMOTE_PATH_2="${RCLONE_DESTINATION_2}${REMOVE_DOWNLOAD_PATH%%/*}"
     CLEAN_UP
     UPLOAD
     exit 0
 elif [ "${TOP_PATH}" != "${FILE_PATH}" ] && [ $2 -eq 1 ]; then # 第三方度盘工具下载（子文件夹或多级目录等情况下的单文件下载）、BT下载（文件夹内文件数等于1），移动文件到设定的网盘文件夹下的相同路径文件夹。
     UPLOAD_PATH="${FILE_PATH}"
     REMOTE_PATH="${RCLONE_DESTINATION}/${REMOVE_DOWNLOAD_PATH%/*}"
+    REMOTE_PATH_2="${RCLONE_DESTINATION_2}/${REMOVE_DOWNLOAD_PATH%/*}"
     UPLOAD
     exit 0
 fi
