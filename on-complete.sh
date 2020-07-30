17efcd4d31d9d8e4ed7d76fb77ba5e38700906a7
Zealer Chen <xinxin8816@gmail.com>
2020-07-30 13:17:46 +0800

    1

--- on-complete.sh
+++ on-complete.sh
@@ -47,7 +47,7 @@ RETRY_NUM=3
 
 #============================================================
 
-DOWNLOAD_PATH='downloads'
+DOWNLOAD_PATH='/app/downloads'
 FILE_PATH=$3                                          # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
 REMOVE_DOWNLOAD_PATH=${FILE_PATH#${DOWNLOAD_PATH}/}   # 路径转换，去掉开头的下载路径。
 TOP_PATH=${DOWNLOAD_PATH}/${REMOVE_DOWNLOAD_PATH%%/*} # 路径转换，BT下载文件夹时为顶层文件夹路径，普通单文件下载时与文件路径相同。
